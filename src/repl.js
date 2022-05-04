42a38a2c0be8ce155587622d772b99482b18570d
akshit0201 <akshit0201@gmail.com>
2022-05-04 08:44:52 +0400

    Added REPL

--- src/repl.js
+++ src/repl.js
@@ -3,7 +3,24 @@ const chalk = require('chalk');
 
 const { parseAndEvaluate } = require('./parse-and-evaluate');
 
-const repl = async () => {};
+const repl = async () => {
+  try{
+    const ans = await askq()
+    const {COMMAND} = ans
+    if (COMMAND.trim()){
+      console.log(chalk.red(parseAndEvaluate(COMMAND)))
+    }
+  }catch (e){
+    console.error(e)
+  }
+  repl()
+};
+
+const askq = () => {
+  const q = [{name: 'COMMAND',type:'input',message: chalk.bgCyan('>')}]
+  return prompt(q)
+
+}
 
 if (require.main === module) {
   console.log(
